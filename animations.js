d19e8edca763c69460295f62f95269ff71897efb
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-01-28 16:24:32 -0500

    readding js

---
+++ animations.js
@@ -0,0 +1,19 @@
+// header and menu animations
+document.addEventListener('DOMContentLoaded', () => {
+  
+    const nav = document.getElementById('header-top');
+    const menuCont = document.getElementById('links-contain');
+    const menu = document.getElementById('links');
+    const bookmark = document.getElementById('bookmark');
+  
+
+      bookmark.addEventListener('click', () => {
+        nav.classList.toggle('color');
+        menuCont.classList.toggle('height');
+        menuCont.classList.toggle('color2');
+        menu.classList.toggle('visibility');
+        bookmark.classList.toggle('heightB');
+      });
+
+  });
+  
