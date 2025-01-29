4c65eb050c5fd806f5492a2d89a90dd4260d091b
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-01-29 14:55:10 -0500

    working on stuff

--- javascript/navsize.js
+++ javascript/navsize.js
@@ -1,30 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
     const navbar = document.getElementById('header-top');
     const bookmark = document.getElementById('bookmark');
     const olinkbar = document.getElementById('links-contain');
     const olinks = document.getElementById('links');
+    const logo = document.getElementById('stashey');
 
-
-    function mobile() {
+    function mobile() { 
         navbar.style.height = "80px";
-        bookmark.style.right = '20%';
-        olinkbar.style.display = 'none';
-        olinks.style.display = 'none';
+        bookmark.style.right = "8%";
+        olinkbar.style.display = "none";
+        olinks.style.display = "none";
+        logo.style.alignItems = "left";
+        logo.style.marginLeft = "10px";
+        logo.style.marginTop = "17px";
+        bookmark.style.height = "110px";
+
     }
 
-    if (document.documentElement.clientWidth < 930) {
+    function desktop() {
+        if (!navbar || !bookmark || !olinkbar || !olinks) return;
+        navbar.style.height = "";
+        bookmark.style.right = ""; 
+        olinkbar.style.display = "";
+        olinks.style.display = "";
+        logo.style.alignItems = "";
+        logo.style.marginLeft = "";
+        logo.style.marginTop = "";
+        bookmark.style.height = "";
+    }
+
+   
+    if (document.documentElement.clientWidth < 1000) {
         mobile();
     } else {
         desktop();
     }
-    
+
+
     window.addEventListener("resize", () => {
-        if (document.documentElement.clientWidth < 930) {
+        if (document.documentElement.clientWidth < 1000) {
             mobile();
         } else {
             desktop();
         }
+    });
     });
 
 
@@ -35,4 +56,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-})
