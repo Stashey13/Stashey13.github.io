17d2459b725b584b1f79da1d56a1ebb0bd24b891
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-02-03 19:06:49 -0500

    m

--- javascript/animations.js
+++ javascript/animations.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('links');
     const bookmark = document.getElementById('bookmark');
     const mobileMenu = document.getElementById('mobileMenu');
+    const text = document.getElementById('stashey');
   
 
     if (document.documentElement.clientWidth > 1000) {
@@ -16,18 +17,28 @@ document.addEventListener('DOMContentLoaded', () => {
         menuCont.classList.toggle('color2');
         menu.classList.toggle('visibility');
         bookmark.classList.toggle('heightB');
+
       });
 
     } else {
-  
-      bookmark.addEventListener('click', () =>  {
 
-      nav.classList.toggle('color');
-      mobileMenu.classList.toggle('visibility2');
+      bookmark.addEventListener('click', () => {
+        nav.classList.toggle('color');
+        mobileMenu.classList.toggle('visibility2');
+        text.classList.toggle('visibility');
     
+        if (mobileMenu.classList.contains('visibility2')) {
+          document.body.style.overflow = 'hidden';
+          document.documentElement.style.overflow = 'hidden';
+        } else {
+          document.body.style.overflow = '';
+          document.documentElement.style.overflow = '';
+        }
 
-      })
+      });
+
     }
+    
 
 
 
