6e5a697ea6a8f385b10a4a5864292c5ce6b69040
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2025-04-11 10:21:09 -0400

    commit

--- javascript/navsize.js
+++ javascript/navsize.js
@@ -6,15 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const olinkbar = document.getElementById('links-contain');
     const olinks = document.getElementById('links');
     const logo = document.getElementById('stashey');
+    const section1text = document.getElementById('wordsss');
 
-    function mobile() { 
+    function mobile1() { 
         navbar.style.height = "80px";
         bookmark.style.right = "8vw";
         olinkbar.style.display = "none";
         olinks.style.display = "none";
         logo.style.marginTop = "17px";
         bookmark.style.height = "110px";
+        section1text.style.width = "600px";
 
+    }
+
+    function mobile2() {
+        section1text.style.width = "300px";
+        section1text.style.minHeight = "none";
     }
 
     function desktop() {
@@ -25,24 +32,32 @@ document.addEventListener('DOMContentLoaded', () => {
         olinks.style.display = "";
         logo.style.marginTop = "";
         bookmark.style.height = "";
+        section1text.style.width = "";
     }
 
-   
-    if (document.documentElement.clientWidth < 1000) {
-        mobile();
-    } else {
-        desktop();
-    }
+
+
+
 
 
     window.addEventListener("resize", () => {
-        if (document.documentElement.clientWidth < 1000) {
-            mobile();
-        } else {
-            desktop();
-        }
-    });
-    });
+            if (document.documentElement.clientWidth < 1000) {
+                mobile1();
+            } else if (document.documentElement.clientWidth < 600) {
+                mobile2();
+            } else {
+                desktop();
+            }
+        });
+    
+
+
+            
+
+        
+        
+});
+
 
 
 
